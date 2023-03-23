ca161b31fc7c3b245d453f32071bc6f4b19a5ba9
minyeob <alsduq1117@naver.com>
2023-03-23 13:11:37 +0900

    알고리즘 시작 문제

---
+++ src/codeplus/algorithm_basic/p10953.java
@@ -0,0 +1,27 @@
+package codeplus.algorithm_basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class p10953 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        String str;
+        StringBuilder sb = new StringBuilder();
+
+        int t = Integer.parseInt(br.readLine());
+
+        for(int i =0;i<t; i++){
+            str = br.readLine();
+            st = new StringTokenizer(str, ",");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(a+b).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
---
+++ src/codeplus/algorithm_basic/p11021.java
@@ -0,0 +1,27 @@
+package codeplus.algorithm_basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class p11021 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        String str;
+
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            str = br.readLine();
+            st = new StringTokenizer(str, " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            sb.append("Case #" + (i + 1) + ": " + (a + b)).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
