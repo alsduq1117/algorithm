8cf70ffc03a3a7ef569538fb48ba7afbcfcd961a
alsduq1117 <alsduq1117@naver.com>
2023-04-04 00:08:37 +0900

    algorithm : 오큰수

--- src/codeplus/algorithm_basic/datastructure/p17298.java
+++ src/codeplus/algorithm_basic/datastructure/p17298.java
@@ -11,22 +11,42 @@ public class p17298 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
+
+
         int t = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine()," ");
+        st = new StringTokenizer(br.readLine(), " ");
+
 
         int[] array = new int[t];
-        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> stack = new Stack<>();  // 오큰수를 찾지못한 array의 인덱스값
 
-        for(int i=0; i<t; i++) {
+        int[] nge = new int[t];
+
+        for (int i = 0; i < t; i++) {
             array[i] = Integer.parseInt(st.nextToken());
         }
 
-        for(int i=0; i<t; i++){
-            if(stack.empty()){
-
+        for (int i = 0; i < t; i++) {
+            if (stack.empty()) {
+                stack.push(i);
+            } else {
+                while (!stack.empty() && array[stack.peek()] < array[i]) {
+                    nge[stack.peek()] = array[i];
+                    stack.pop();
+                }
+                stack.push(i);
             }
         }
+        while (!stack.empty()) {
+            nge[stack.pop()] = -1;
+        }
 
-
+        for (int i = 0; i < nge.length; i++) {
+            sb.append(nge[i] + " ");
+        }
+        System.out.println(sb);
     }
 }
+
+
+//써야함
