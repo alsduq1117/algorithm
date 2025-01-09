c9360df968870531feabb6875885c449e8deeafa
alsduq1117 <alsduq1117@naver.com>
2025-01-10 01:10:36 +0900

    hash : 신고 결과 받기

--- src/codingtest_java/hash/Problem21.java
+++ src/codingtest_java/hash/Problem21.java
@@ -1,47 +1,36 @@
 package codingtest_java.hash;
 
-import java.util.HashMap;
+import java.util.*;
 
 public class Problem21 {
 
-    public static void main(String[] args) {
-        String[] want = {"kim", "min", "yeob"};
-        int[] number = {3, 2, 2};
-        String[] discount = {
-                "kim", "kim", "kim", "min", "min",
-                "yeob", "yeob"
-        };
+    public int[] solution(String[] id_list, String[] report, int k) {
+        List<Integer> mailList = new ArrayList<>();
+        Map<String, Integer> reportMailCount = new HashMap<>(); // 신고 메일을 count 할 HashMap
+        Map<String, HashSet<String>> reportHashMap = new HashMap<>(); //신고 이력이 담긴 HashMap <신고당한 사람 , 신고한 사람>
 
-        int result = solution(want, number, discount);
-        System.out.println("결과: " + result); // 결과 출력
+        for (String reportHistory : report) {
+            StringTokenizer st = new StringTokenizer(reportHistory, " ");
+            String reportId = st.nextToken();   //신고한 id
+            String reportedId = st.nextToken();  // 신고당한 id
 
-    }
-
-    public static int solution(String[] want, int[] number, String[] discount) {
-        HashMap<String, Integer> wantHashMap = new HashMap<>();
-        int answer = 0;
-        int numberSum = 0;   // 원하는 물품의 갯수의 함
-
-        for(int i=0; i<want.length; i++){
-            wantHashMap.put(want[i], number[i]);
+            reportHashMap.putIfAbsent(reportedId, new HashSet<>()); // 신고당한 사람이 처음 신고된 경우 HashSet 생성
+            reportHashMap.get(reportedId).add(reportId); // 신고한 사람 추가
         }
 
-        for(int i=0; i< number.length; i++){
-            numberSum += number[i];
-        }
-
-
-        for(int i=0; i<= discount.length - numberSum; i++){
-            HashMap<String, Integer> discountHashMap = new HashMap<>();
-            for(int j=i; j < i + numberSum; j++){
-                discountHashMap.put(discount[j],discountHashMap.getOrDefault(discount[j],0) + 1);
-            }
-
-            if(wantHashMap.equals(discountHashMap)){
-                answer += 1;
+        for (String Id : reportHashMap.keySet()) {
+            if (reportHashMap.get(Id).size() >= k) {   //k 번 이상 신고당한 Id 라면?
+                HashSet<String> reportIds = reportHashMap.get(Id); // 신고이력을 통해 해당 Id 를 신고한 Id들을 추출
+                for (String reportId : reportIds) {
+                    reportMailCount.put(reportId, reportMailCount.getOrDefault(reportId, 0) + 1);  //신고한 Id를 기반으로 메일 카운트
+                }
             }
         }
 
-        return answer;
+        for (String id : id_list) {
+            mailList.add(reportMailCount.getOrDefault(id, 0));
+        }
+
+        return mailList.stream().mapToInt(i -> i).toArray();
     }
 }
